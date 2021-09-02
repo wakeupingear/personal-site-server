92388503a3778f21ab7fc1fbe0a5f7156173dcc4
Will Farhat <willf668@gmail.com>
2021-09-01 21:46:43 -0700

    auth bug fixes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const basicAuth = require('express-basic-auth');
 const path = require('path');
 const cors = require('cors');
 const { exec } = require('child_process');
+const os = require('os');
 
 const publicIp = require('public-ip');
 let IPV4 = "POOP";
@@ -13,20 +14,20 @@ publicIp.v4().then(ip => {
     IPV4 = ip;
 });
 
-let siteOptions = {},apiOptions={};
-let sitePort=443, apiPort=5000;
+let siteOptions = {}, apiOptions = {};
+let sitePort = 443, apiPort = 5000;
 try {
     siteOptions.key = fs.readFileSync('/etc/letsencrypt/live/willfarhat.com/privkey.pem');
     siteOptions.cert = fs.readFileSync('/etc/letsencrypt/live/willfarhat.com/cert.pem');
-	siteOptions.ca = fs.readFileSync('/etc/letsencrypt/live/willfarhat.com/chain.pem');
-	
-	apiOptions.key = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/privkey.pem');
-	apiOptions.cert = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/cert.pem');
-        apiOptions.ca = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/chain.pem');
+    siteOptions.ca = fs.readFileSync('/etc/letsencrypt/live/willfarhat.com/chain.pem');
+
+    apiOptions.key = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/privkey.pem');
+    apiOptions.cert = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/cert.pem');
+    apiOptions.ca = fs.readFileSync('/etc/letsencrypt/live/api.willfarhat.com/chain.pem');
 }
 catch {
     console.log("No SSL files found, falling back to HTTP");
-    sitePort=80;
+    sitePort = 80;
 }
 
 let auth = "";
@@ -37,27 +38,32 @@ fs.readFile(__dirname + '/password.txt', function (err, data) {
     auth = ("admin:" + data.toString()).replace(/[^\x00-\x7F]/g, "").replace(/(\r\n|\n|\r)/gm, "");
 });
 
-const reactApp = express();
-const reactDir = (process.argv.length > 2 ? process.argv[2] : path.resolve("../personal-site-21/"));
-reactApp.use(express.static(path.join(reactDir, 'build')));
-reactApp.get('/.well-known/acme-challenge/9dQPhqJkntU8ttUeIL6EOM2w8gF2gPnArJtXnmzMvrw',function(req,res){
-	res.sendFile('/home/pi/personal-site-server/a-challenge');
-});
-reactApp.get('/.well-known/acme-challenge/OnvDPYbfbx_Ldu0JZVmYITFIBoPkDE5gdK7IoM8M0u8',function(req,res){
-	res.sendFile('/home/pi/personal-site-server/b-challenge');
-});
-reactApp.get('*', function (req, res) {
-    res.sendFile(reactDir + "/build/index.html");
-});
-https.createServer(siteOptions, reactApp).listen(sitePort);
+let localTest = (os.userInfo().username !== "pi");
+if (!localTest) {
+    const reactApp = express();
+    const reactDir = (process.argv.length > 2 ? process.argv[2] : path.resolve("../personal-site-21/"));
+    reactApp.use(express.static(path.join(reactDir, 'build')));
+    reactApp.get('/.well-known/acme-challenge/9dQPhqJkntU8ttUeIL6EOM2w8gF2gPnArJtXnmzMvrw', function (req, res) {
+        res.sendFile('/home/pi/personal-site-server/a-challenge');
+    });
+    reactApp.get('/.well-known/acme-challenge/OnvDPYbfbx_Ldu0JZVmYITFIBoPkDE5gdK7IoM8M0u8', function (req, res) {
+        res.sendFile('/home/pi/personal-site-server/b-challenge');
+    });
+    reactApp.get('*', function (req, res) {
+        res.sendFile(reactDir + "/build/index.html");
+    });
+    https.createServer(siteOptions, reactApp).listen(sitePort);
+}
 
 const apiApp = express();
 apiApp.use(cors({
     origin: '*'
 }));
 apiApp.use((req, res, next) => {
-const newAuth=((new Buffer(req.headers.authorization,"base64")).toString("ascii")).replace(/[^\x00-\x7F]/g, "").replace(/(\r\n|\n|\r)/gm, "");
-if (auth != newAuth) {
+    console.log(req);
+    let newAuth = "";
+    if (req.headers.authorization !== undefined) newAuth = ((Buffer.from(req.headers.authorization, "base64")).toString("ascii")).replace(/[^\x00-\x7F]/g, "").replace(/(\r\n|\n|\r)/gm, "");
+    if (auth != newAuth) {
         res.send({ data: false });
         return;
     }
@@ -69,4 +75,5 @@ apiApp.get('/reset', function (req, res) {
 apiApp.get('/ip', function (req, res) {
     res.send({ data: IPV4 });
 });
-https.createServer(apiOptions, apiApp).listen(apiPort);
+if (!localTest) https.createServer(apiOptions, apiApp).listen(apiPort);
+else apiApp.listen(apiPort);
